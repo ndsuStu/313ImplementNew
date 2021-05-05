cd8bb278ae6abf57394bf8481db8d4d347a528cd
MichaelSullivan2 <76757413+MichaelSullivan2@users.noreply.github.com>
2021-05-05 15:37:10 -0500

    Trying to upload work, attempt two.
    Trying to upload my components.

--- gpImplement/src/app/app.module.ts
+++ gpImplement/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CreateCustAcctComponent } from './create-cust-acct/create-cust-acct.component';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+import {MatCardModule} from '@angular/material/card';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { TabberComponent } from './tabber/tabber.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import { ConnectingService } from './connecting.service';
+import { EditComponent } from './edit/edit.component';
 @NgModule({
   declarations: [
     AppComponent,
+    SearchFormComponent,
+    TabberComponent,
+    EditComponent,
     CreateCustAcctComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatDividerModule
   ],
-  providers: [],
+  providers: [ConnectingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
